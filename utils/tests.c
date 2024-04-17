6b7a4b0e7c91fa9338b60e819c6dae53f0afef0b
ABDURRAHMAN <123483885+48k483x@users.noreply.github.com>
2024-04-17 13:01:05 +0000

    MAYBE THE PARSING IS DONE 23%

--- utils/tests.c
+++ utils/tests.c
@@ -10,7 +10,7 @@ void    copy(t_tiny *tiny)
     {
         if (_strcmp(tiny->full_cmd[i], "|") == 0)
         {
-            tiny->cmds[j] = malloc(sizeof(char *) * (i - k + 1));
+            tiny->cmds[j] = malloc(sizeof(char **) * (i - k + 1));
             if (!tiny->cmds[j])
                 return ;
             while (k < i)
@@ -19,8 +19,7 @@ void    copy(t_tiny *tiny)
                 k++;
                 x++;
             }
-            printf("\n");
-            tiny->cmds[j][k] = NULL;
+            tiny->cmds[j][x] = NULL;
             j++;
             k = i + 1;
             x = 0;
@@ -29,20 +28,21 @@ void    copy(t_tiny *tiny)
     }
     printf("K = %d\n", k);
     printf("i = %d\n", i);
-    while (tiny->full_cmd[k] != NULL)
+    printf("j = %d\n", j);
+    printf("\nfull_cmd[%d] = %s\n\n\n", i, tiny->full_cmd[k]);
+    tiny->cmds[j] = malloc(sizeof(char *) * (i - k + 1));
+    if (!tiny->cmds[j])
+        return ;
+    while (tiny->full_cmd[k])
     {
-        tiny->cmds[j] = malloc(sizeof(char *) * (i - k + 1));
-        if (!tiny->cmds[j])
-            return ;
-        while (tiny->full_cmd[k] != NULL)
-        {
-            tiny->cmds[j][x] = strdup(tiny->full_cmd[k]);
-            printf("cmd[%d][%d] = %s\n", j, x, tiny->cmds[j][x]);
-            k++;
-            x++;
-        }
-        tiny->cmds[j][x] = NULL;
+        tiny->cmds[j][x] = strdup(tiny->full_cmd[k]);
+        printf("tiny->cmds[%d][%d] = %s\n", j, x, tiny->cmds[j][x]);
+        k++;
+        x++;
+        printf("k = %d\n", k);
     }
+    tiny->cmds[j][x] = NULL;
+    j++;
     tiny->cmds[j] = NULL;
 }
 
@@ -53,7 +53,7 @@ void    pipe_handle(t_tiny *tiny)
     int k = 0;
     int cmd_count = 0;
 
-    char *cmd = "ls | wc -l | cat -e | grep 'mini shell' | echo 'hello world' ";
+    char *cmd = "/bin/echo  ana | wc -l | echo yes | hello | ls";
     while (cmd[i] != '\0')
     {
         if (cmd[i] == '|')
@@ -70,7 +70,7 @@ void    pipe_handle(t_tiny *tiny)
         if (_strcmp(tiny->full_cmd[i], "|") == 0)
         {
             while (++j < i)
-                printf("%s", tiny->full_cmd[j]);
+                printf("%s ", tiny->full_cmd[j]);
             printf("\n");
             j = i;
         }
@@ -78,7 +78,7 @@ void    pipe_handle(t_tiny *tiny)
     }
     j++;
     while (tiny->full_cmd[j] != NULL)
-        printf("%s", tiny->full_cmd[j++]);
+        printf("%s ", tiny->full_cmd[j++]);
     printf("\n");
 
     copy(tiny);
@@ -92,9 +92,21 @@ void    pipe_handle(t_tiny *tiny)
     }
     
 }
+
+void fork_s(t_tiny *tiny)
+{
+    pid_t pid;
+    int i = 0;
+        if (!(pid = fork()))
+            execve(tiny->cmds[i][0], tiny->cmds[i], NULL);
+        else
+            waitpid(pid, NULL, 0);
+}
 int main (int ac, char **av)
 {
     (void)ac; (void)av; t_tiny tiny;
 
     pipe_handle(&tiny);
+    
+    fork_s(&tiny);
 }
