51fdea04710b734cde12b0baef828b8287be264a
FROST-48K483X <romaissaeabde@gmail.com>
2024-05-17 17:27:56 +0100

    norminette fl exec done, fs ba9i 4er lheader

--- src/exec/bin.c
+++ src/exec/bin.c
@@ -1,63 +1,59 @@
 #include "../../minishell.h"
 
-
-char **two_dx(t_token *start)
+char	**two_dx(t_token *start)
 {
-    t_token *token;
-    char **str;
-    int i;
+	t_token	*token;
+	char	**str;
+	int		i;
 
-    if (!start)
-        return (NULL);
-    token = start->next;
-    i = 1;
-    while (token && token->type < TRUNC)
-    {
-        token = token->next;
-        i++;
-    }
-    str = _malloc(sizeof(char *) * (i + 1));
-    str[0] = start->str;
-    i = 1;
-    token = start->next;
-    while (token && token->type < TRUNC)
-    {
-        str[i] = token->str;
-        token = token->next;
-        i++;
-    }
-    str[i] = NULL;
-    return (str);
+	if (!start)
+		return (NULL);
+	token = start->next;
+	i = 1;
+	while (token && token->type < TRUNC)
+	{
+		token = token->next;
+		i++;
+	}
+	str = _malloc(sizeof(char *) * (i + 1));
+	str[0] = start->str;
+	i = 1;
+	token = start->next;
+	while (token && token->type < TRUNC)
+	{
+		str[i] = token->str;
+		token = token->next;
+		i++;
+	}
+	str[i] = NULL;
+	return (str);
 }
 
-
-char **path_checker(t_token *token)
+char	**path_checker(t_token *token)
 {
-    char  **str;
-    char *path;
-    char **env;
-    int i = -1;
+	char	**str;
+	char	*path;
+	char	**env;
+	int		i;
 
-    str = two_dx(token);
-    i = 0;
-    env = _split(getenv("PATH"), ':');
-    if (access(str[0], X_OK) == 0)
-        str[0] = str[0];
-    else
-    {
-        while (env && env[i])
-        {
-            path = _strcat(env[i], "/");
-            path = _strcat(path, str[0]);
-            if (access(path, X_OK) == 0)
-            {
-                str[0] = _strdup(path);
-                _memdel(path);
-            }
-            i++;
-        }
-    }
-    free_split(env);
-    return (str);
+	i = -1;
+	str = two_dx(token);
+	env = _split(getenv("PATH"), ':');
+	if (access(str[0], X_OK) == 0)
+		str[0] = str[0];
+	else
+	{
+		while (env && env[++i])
+		{
+			path = _strcat(env[i], "/");
+			path = _strcat(path, str[0]);
+			if (access(path, X_OK) == 0)
+			{
+				str[0] = _strdup(path);
+				_memdel(path);
+			}
+		}
+	}
+	free_split(env);
+	return (str);
 }
-
--- src/exec/redir.c
+++ src/exec/redir.c
@@ -1,95 +1,93 @@
 #include "../../minishell.h"
 
-void redir(t_tiny *tiny, t_token *token, int type)
+void	redir(t_tiny *tiny, t_token *token, int type)
 {
-    // printf("hadda li f redir token->next->str: %s\n", token->str);
-    signal(SIGINT, int_handler);
-    _close(tiny->fdout);
-    if (type == TRUNC)
-        tiny->fdout = open(token->str, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    else if (type == APPEND)
-        tiny->fdout = open(token->str, O_CREAT | O_WRONLY | O_APPEND, 0644);
-    if (tiny->fdout < 0)
-    {
-        _perror("tiny: ");
-        _perror(token->str);
-        printsdr(": No such file or directory");
-        tiny->ret = 1;
-        tiny->no_exec = 1;
-        return ;
-    }
-    dup2(tiny->fdout, STDOUT);
+	signal(SIGINT, int_handler);
+	_close(tiny->fdout);
+	if (type == TRUNC)
+		tiny->fdout = open(token->str, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	else if (type == APPEND)
+		tiny->fdout = open(token->str, O_CREAT | O_WRONLY | O_APPEND, 0644);
+	if (tiny->fdout < 0)
+	{
+		_perror("tiny: ");
+		_perror(token->str);
+		printsdr(": No such file or directory");
+		tiny->ret = 1;
+		tiny->no_exec = 1;
+		return ;
+	}
+	dup2(tiny->fdout, STDOUT);
 }
 
-void    input(t_tiny *tiny, t_token *token)
+void	input(t_tiny *tiny, t_token *token)
 {
-    
-    _close(tiny->fdin);
-    tiny->fdin = open(token->str, O_RDONLY, 0644);
-    if (tiny->fdin == -1)
-    {
-        _perror("tiny : ");
-        _perror(token->str);
-        printsdr(": No such file or directory");
-        tiny->ret = 1;
-        tiny->no_exec = 1;
-        return ;
-    }
-    dup2(tiny->fdin, STDIN);
+	_close(tiny->fdin);
+	tiny->fdin = open(token->str, O_RDONLY, 0644);
+	if (tiny->fdin == -1)
+	{
+		_perror("tiny : ");
+		_perror(token->str);
+		printsdr(": No such file or directory");
+		tiny->ret = 1;
+		tiny->no_exec = 1;
+		return ;
+	}
+	dup2(tiny->fdin, STDIN);
 }
 
-void redir_her_doc(t_tiny *tiny, t_token *token)
+void	redir_her_doc(t_tiny *tiny, t_token *token)
 {
-    char *line;
+	char	*line;
 
-    _close(tiny->fdin);
-    tiny->fdin = open(".heredoc", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    while (1)
-    {
-        line = readline(" > ");
-        if (!line)
-        {
-            _memdel(line);
-            break ;
-        }
-        if (_strcmp(line, token->str) == 0)
-        {
-                _memdel(line);
-                break ;
-        }
-        write(tiny->fdin, line, _strlen(line));
-        write(tiny->fdin, "\n", 1);
-    }
-    close(tiny->fdin);
-    tiny->fdin = open(".heredoc", O_RDONLY);
-    dup2(tiny->fdin, STDIN_FILENO);
-    unlink(".heredoc");
+	_close(tiny->fdin);
+	tiny->fdin = open(".heredoc", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	while (1)
+	{
+		line = readline(" > ");
+		if (!line)
+		{
+			_memdel(line);
+			break ;
+		}
+		if (_strcmp(line, token->str) == 0)
+		{
+			_memdel(line);
+			break ;
+		}
+		write(tiny->fdin, line, _strlen(line));
+		write(tiny->fdin, "\n", 1);
+	}
+	close(tiny->fdin);
+	tiny->fdin = open(".heredoc", O_RDONLY);
+	dup2(tiny->fdin, STDIN_FILENO);
+	unlink(".heredoc");
 }
 
-int tinypipe(t_tiny *tiny)
+int	tinypipe(t_tiny *tiny)
 {
-    int fd[2];
-    pid_t pid;
+	int		fd[2];
+	pid_t	pid;
 
-    pipe(fd);
-    pid = fork();
-    if (pid == 0)
-    {
-        _close(fd[1]);
-        dup2(fd[0], STDIN);
-        tiny->pipin = fd[0];
-        tiny->pid = -1;
-        tiny->parent = 0;
-        tiny->no_exec = 0;
-        return (2);
-    }
-    else
-    {
-        _close(fd[0]);
-        dup2(fd[1], STDOUT);
-        tiny->pipout = fd[1];
-        tiny->pid = pid;
-        tiny->last = 0;
-        return (1);
-    }
+	pipe(fd);
+	pid = fork();
+	if (pid == 0)
+	{
+		_close(fd[1]);
+		dup2(fd[0], STDIN);
+		tiny->pipin = fd[0];
+		tiny->pid = -1;
+		tiny->parent = 0;
+		tiny->no_exec = 0;
+		return (2);
+	}
+	else
+	{
+		_close(fd[0]);
+		dup2(fd[1], STDOUT);
+		tiny->pipout = fd[1];
+		tiny->pid = pid;
+		tiny->last = 0;
+		return (1);
+	}
 }
