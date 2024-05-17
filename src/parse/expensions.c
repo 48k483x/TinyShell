d07647e00602339a80533050eccae99d819c13ad
FROST-48K483X <romaissaeabde@gmail.com>
2024-05-17 17:35:09 +0100

    norminette fl parse   fs ba9i 4er lheader

--- src/parse/expensions.c
+++ src/parse/expensions.c
@@ -1,25 +1,25 @@
 #include "../../minishell.h"
 
-
-int		varlcpy(char *new_arg, const char *env_value, int pos)
+int	varlcpy(char *new_arg, const char *env_value, int pos)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (env_value[i])
 		new_arg[pos++] = env_value[i++];
 	return (i);
 }
-void		insert_var(t_expansions *ex, char *arg, t_env *env, int ret)
+
+void	insert_var(t_expansions *ex, char *arg, t_env *env, int ret)
 {
 	char	*env_value;
 
 	env_value = get_var_value(arg, ex->j, env, ret);
 	if (env_value)
-        ex->i += varlcpy(ex->new_arg, env_value, ex->i);
+		ex->i += varlcpy(ex->new_arg, env_value, ex->i);
 	_memdel(env_value);
 	if (arg[ex->j] == '?')
-        ex->j++;
+		ex->j++;
 	if (_isdigit(arg[ex->j]) == 0 && arg[ex->j - 1] != '?')
 	{
 		while (is_env_char(arg[ex->j]) == 1)
@@ -32,7 +32,7 @@ void		insert_var(t_expansions *ex, char *arg, t_env *env, int ret)
 	}
 }
 
-char			*expansions(char *arg, t_env *env, int ret)
+char	*expansions(char *arg, t_env *env, int ret)
 {
 	t_expansions	ex;
 	int				new_arg_len;
@@ -43,11 +43,11 @@ char			*expansions(char *arg, t_env *env, int ret)
 	ex.j = 0;
 	while (ex.i < new_arg_len && arg[ex.j])
 	{
-		while (arg[ex.j] == EXPANSION )
+		while (arg[ex.j] == EXPANSION)
 		{
 			ex.j++;
-			if ((arg[ex.j] == '\0' || _isalnum(arg[ex.j]) == 0)
-			&& arg[ex.j] != '?')
+			if ((arg[ex.j] == '\0' || _isalnum(arg[ex.j]) == 0) && \
+					arg[ex.j] != '?')
 				ex.new_arg[ex.i++] = '$';
 			else
 				insert_var(&ex, arg, env, ret);
--- src/parse/parse.c
+++ src/parse/parse.c
@@ -1,92 +1,93 @@
 #include "../../minishell.h"
 
-int quotes(char *s, int index)
+int	quotes(char *s, int index)
 {
-    int quote;
-    int i;
+	int	quote;
+	int	i;
 
-    quote = 0;
-    i = 0;
-    while (s[i] && i != index)
-    {
-        if (i > 0 && s[i - 1] == '\\')
-            ;
-        else if (quote == 0 && s[i] == '\'')
-            quote = 1;
-        else if (quote == 0 && s[i] == '"')
-            quote = 2;
-        else if (quote == 1 && s[i] == '\'')
-            quote = 0;
-        else if (quote == 2 && s[i] == '"')
-            quote = 0;
-        i++;
-    }
-    return (quote);
+	quote = 0;
+	i = 0;
+	while (s[i] && i != index)
+	{
+		if (i > 0 && s[i - 1] == '\\')
+			;
+		else if (quote == 0 && s[i] == '\'')
+			quote = 1;
+		else if (quote == 0 && s[i] == '"')
+			quote = 2;
+		else if (quote == 1 && s[i] == '\'')
+			quote = 0;
+		else if (quote == 2 && s[i] == '"')
+			quote = 0;
+		i++;
+	}
+	return (quote);
 }
 
-int is_operator(char *s, int index)
+int	is_operator(char *s, int index)
 {
-    if (index > 0 && _strchr("|<>;", s[index]))
-        return (1);
-    if (_strchr("|<>;", s[index]) && quotes(s, index) == 0)
-        return (1);
-    else
-        return (0);
+	if (index > 0 && _strchr("|<>;", s[index]))
+		return (1);
+	if (_strchr("|<>;", s[index]) && quotes(s, index) == 0)
+		return (1);
+	else
+		return (0);
 }
 
-char *spacing(char *s)
+char	*spacing(char *s)
 {
-    char *new;
-    int i;
-    int count;
+	char	*new;
+	int		i;
+	int		count;
 
-    i = 0;
-    count = 0;
-    while (s[i])
-    {
-        if (is_operator(s, i))
-            count++;
-        i++;
-    }
-    new = _malloc(sizeof(char) * (i + 1 + count * 2));
-    return (new);
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (is_operator(s, i))
+			count++;
+		i++;
+	}
+	new = _malloc(sizeof(char) * (i + 1 + count * 2));
+	return (new);
 }
 
-char *space_line(char *line)
+char	*space_line(char *line)
 {
-    char *new;
-    int i;
-    int j;
+	char	*new;
+	int		i;
+	int		j;
 
-    i = 0;
-    j = 0;
-    new = spacing(line);
-    while (new  && line[i])
-    {
-        if (line[i] == '$' && (i - 1) && line[i - 1] == '\'')
-            new[j++] = line[i++];
-        else if (line[i] == '$' && i && line[i - 1] != '\\')
-            new[j++] = (char)(-line[i++]);
-        else if (quotes(line, i) == 0 && is_operator(line, i))
-        {
-            new[j++] = ' ';
-            new[j++] = line[i++];
-            if (quotes(line, i) == 0 && (line[i] == '<' || line[i] == '>'))
-                new[j++] = line[i++];
-            new[j++] = ' ';
-        }
-        else
-            new[j++] = line[i++];
-    }
-    new[j] = '\0';
-    _memdel(line);
-    return (new);
+	i = 0;
+	j = 0;
+	new = spacing(line);
+	while (new && line[i])
+	{
+		if (line[i] == '$' && (i - 1) && line[i - 1] == '\'')
+			new[j++] = line[i++];
+		else if (line[i] == '$' && i && line[i - 1] != '\\')
+			new[j++] = (char)(-line[i++]);
+		else if (quotes(line, i) == 0 && is_operator(line, i))
+		{
+			new[j++] = ' ';
+			new[j++] = line[i++];
+			if (quotes(line, i) == 0 && (line[i] == '<' || line[i] == '>'))
+				new[j++] = line[i++];
+			new[j++] = ' ';
+		}
+		else
+			new[j++] = line[i++];
+	}
+	new[j] = '\0';
+	_memdel(line);
+	return (new);
 }
-void parse(t_tiny *tiny)
+
+void	parse(t_tiny *tiny)
 {
 	tiny->line = space_line(tiny->line);
-	if (tiny->line &&tiny->line[0] == '$')
-	    tiny->line[0] = (char)(-tiny->line[0]);
+	if (tiny->line && tiny->line[0] == '$')
+		tiny->line[0] = (char)(-tiny->line[0]);
 	tiny->start = get_token(tiny->line);
-    _memdel(tiny->line);
+	_memdel(tiny->line);
 }
