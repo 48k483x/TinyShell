17b73c5a8fc7cbcc44c7f074192607e61f0f5e9f
FROST-48K483X <romaissaeabde@gmail.com>
2024-05-17 17:28:58 +0100

    norminette fl free  done, fs ba9i 4er lheader

--- src/free/free.c
+++ src/free/free.c
@@ -1,22 +1,21 @@
 #include "../../minishell.h"
 
-void free_env(t_env *env)
+void	free_env(t_env *env)
 {
-    t_env *tmp;
+	t_env	*tmp;
 
-    while (env)
-    {
-        tmp = env;
-        env = env->next;
-        _memdel(tmp->value);
-        _memdel(tmp);
-        tmp = NULL;
-    }
+	while (env)
+	{
+		tmp = env;
+		env = env->next;
+		_memdel(tmp->value);
+		_memdel(tmp);
+		tmp = NULL;
+	}
 }
 
-void free_all(t_tiny *tiny)
+void	free_all(t_tiny *tiny)
 {
-
-    free_env(tiny->env);
-    free_env(tiny->private_env);
+	free_env(tiny->env);
+	free_env(tiny->private_env);
 }
